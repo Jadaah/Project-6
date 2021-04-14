4577f6c60a9466d4850553e74effe11b51eef73e
Jadaah <>
2021-04-14 06:57:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+
+   String[] firstName = {"lexi" , "lilly" , "tyler" , "brent" , "keely" , "ryan" , "stephany"};
+  }
+   Scanner scan = new Scanner;
+   System.out.println("Enter your name");
+   String fullName = scan.nextLine();
+   {
+     int index;
+     index = fullname.indexOf(" ");
+     System.out.println(index);
+
+     String name;
+     name = fullname.substring(0,index);
+     System.out.println(name);
+   
+   for(int i = 0; i < firstName.length; i++); 
+   if(name.equals(firstName[])){
+     System.out.println("Hey guys" + " " + fullName);
+   }
+   }
+
+
+
+
+  }
